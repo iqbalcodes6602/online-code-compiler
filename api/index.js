1f973f2f78d67fc653e3a7b95a5a615b718ac3e5
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2024-06-18 18:26:41 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+
+const { generateFile } = require("./generateFile");
+const { executePy } = require("./executePy");
+const { deleteFile } = require("./generateFile");
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ hello: "world!" });
+});
+
+app.post("/run", async (req, res) => {
+  const { language = "py", code } = req.body;
+
+  if (code === undefined) {
+    return res.status(400).json({ success: false, error: "Empty code body!" });
+  }
+  try {
+    // need to generate a c++ file with content from the request
+    const filepath = await generateFile(language, code);
+    // we need to run the file and send the response
+    const output = await executePy(filepath);
+    // delete the file after execution
+    await deleteFile(filepath);
+
+    return res.json({ filepath, output });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000!`);
+});
