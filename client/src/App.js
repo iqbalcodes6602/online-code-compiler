6daa144b2810d5bef64bd52de0f9b65198684d4c
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2024-06-22 22:54:41 +0530

    Refactor code execution functions for JavaScript and Python

--- client/src/App.js
+++ client/src/App.js
@@ -67,90 +67,6 @@ function App() {
   };
 
   return (
-    // <div className="h-screen bg-gray-900 flex flex-col">
-    //   {/* Header */}
-    //   <header className="text-white flex items-center justify-center p-2">
-    //     <select
-    //       className="ml-4 px-4 py-2 bg-gray-700 text-white rounded"
-    //       value={language}
-    //       onChange={handleLanguageChange}
-    //     >
-    //       {Object.keys(LANGUAGE_VERSIONS).map((lang) => (
-    //         <option key={lang} value={lang}>
-    //           {lang}
-    //         </option>
-    //       ))}
-    //     </select>
-    //     Online Code Compiler
-    //     <button className="ml-4 px-4 py-2 bg-blue-500 text-white font-bold rounded cursor-pointer" onClick={handleSubmit}>
-    //       Run Code
-    //     </button>
-    //   </header>
-
-    //   {/* Main Content */}
-    //   <main className="flex-1 grid md:grid-cols-5 gap-4 p-4 text-gray-300 overflow-auto">
-    //     {/* Left Column */}
-    //     <div className="flex flex-col gap-4 md:col-span-3">
-    //       <CodeEditor language={language} code={code} setCode={setCode} />
-    //     </div>
-
-    //     {/* Right Column */}
-    //     <div className="grid grid-rows-3 gap-4 md:col-span-2">
-    //       {/* Input Section */}
-    //       <div className="flex flex-col gap-2 bg-gray-800 p-4 row-span-1">
-    //         <label htmlFor="input-textarea" className="font-bold text-gray-300">Input</label>
-    //         <textarea
-    //           id="input-textarea"
-    //           className="resize-none bg-gray-900 text-gray-300 p-4 rounded outline-none h-[100%]"
-    //           placeholder=""
-    //           value={input}
-    //           onChange={(e) => setInput(e.target.value)}
-    //         />
-    //       </div>
-
-    //       {/* Output Section */}
-    //       <div className="flex flex-col gap-2 bg-gray-800 p-4 row-span-2">
-    //         <label htmlFor="output-div" className="flex font-bold text-gray-300 gap-1">
-    //           Output {executionTime &&
-    //             <span className="text-blue-400">({executionTime})</span>
-    //           }
-    //         </label>
-    //         <textarea
-    //           className="resize-none bg-gray-900 text-gray-300 p-4 rounded outline-none h-[100%]"
-    //           placeholder=""
-    //           value={output || error}
-    //           disabled={true}
-    //         />
-    //       </div>
-    //     </div>
-    //   </main>
-    // </div>
-    // <ResizablePanelGroup
-    //   direction="horizontal"
-    //   className="max-w-md rounded-lg border"
-    // >
-    //   <ResizablePanel defaultSize={50}>
-    //     <div className="flex h-[200px] items-center justify-center p-6">
-    //       <span className="font-semibold">One</span>
-    //     </div>
-    //   </ResizablePanel>
-    //   <ResizableHandle withHandle={true} />
-    //   <ResizablePanel defaultSize={50}>
-    //     <ResizablePanelGroup direction="vertical">
-    //       <ResizablePanel defaultSize={25}>
-    //         <div className="flex h-full items-center justify-center p-6">
-    //           <span className="font-semibold">Two</span>
-    //         </div>
-    //       </ResizablePanel>
-    //       <ResizableHandle withHandle={true} />
-    //       <ResizablePanel defaultSize={75}>
-    //         <div className="flex h-full items-center justify-center p-6">
-    //           <span className="font-semibold">Three</span>
-    //         </div>
-    //       </ResizablePanel>
-    //     </ResizablePanelGroup>
-    //   </ResizablePanel>
-    // </ResizablePanelGroup>
     <div className="h-screen bg-gray-900 flex flex-col">
       {/* Header */}
       <header className="text-white flex items-center justify-center p-2">
@@ -180,7 +96,7 @@ function App() {
               <CodeEditor language={language} code={code} setCode={setCode} />
             </div>
           </ResizablePanel>
-          <ResizableHandle className='bg-gray-900' withHandle />
+          <ResizableHandle className='bg-gray-900 text-gray-900' withHandle />
           {/* Right Column */}
           <ResizablePanel defaultSize={40} minSize={20}>
             <ResizablePanelGroup direction="vertical" className="h-full gap-[0.1px]">
@@ -197,7 +113,7 @@ function App() {
                   />
                 </div>
               </ResizablePanel>
-              <ResizableHandle className='bg-gray-900' withHandle />
+              <ResizableHandle className='bg-gray-900 text-gray-900' withHandle />
               {/* Output Section */}
               <ResizablePanel defaultSize={70} minSize={20}>
                 <div className="flex flex-col gap-2 bg-gray-800 p-2 h-full">
