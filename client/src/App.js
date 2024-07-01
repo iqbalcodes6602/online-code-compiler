f2a410ecb1f2b85d6c976f1ebbeea1c2d6d00e07
iqbal <111554475+noobcoder6602@users.noreply.github.com>
2024-07-01 19:07:09 +0530

    Refactor App.js to remove unused useEffect import

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // frontend/src/App.js
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import CodeEditor from './components/CodeEditor';
 import {
